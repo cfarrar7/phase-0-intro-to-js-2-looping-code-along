6b9415c1b8e7e7345acd7d22a290530e8b8ec479
Clifford <c_farrar@live.com>
2022-09-21 13:34:49 -0700

    commit tree d28cde90240d8950945480035bd3800fe350c144

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message)
+    };
+    return messages;
+}
+function countDown() {
+    let number = 10 ;
+    while ( number > 0){console.log(number--)};
+    console.log(number);
+}
